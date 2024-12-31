53187eb2bd469358b39e09ee3c1e86ee7c1b8901
Daniel Goins <dwgoins98@gmail.com>
2024-12-31 12:42:50 -0500

    Restarting - using Postman now - had API connection issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RolesComponent } from './Components/roles/roles.component';
-import { MasterComponent } from './Components/master/master.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MasterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
